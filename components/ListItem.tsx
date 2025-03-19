7b2be4caa254b7ea7e74ddd2da709366509eba4a
imvbhargav <87763304+imvbhargav@users.noreply.github.com>
2025-03-19 16:24:15 +0530

    Changed home button size

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -8,7 +8,10 @@ function ListItem({icon, title, to, active = false}: Readonly<{icon: string, tit
   return (
     <li className={`text-2xl mb-4 flex gap-6 items-center hover:bg-zinc-800 rounded-xl transition-all duration-300 ${active ? 'bg-slate-800 hover:bg-slate-800 pointer-events-none' : 'bg-transparent'}`}>
       <a href={to} className="relative flex gap-6 items-center w-full pl-2 p-4">
-        <Image src={icon} alt={title} width={35} height={35} />
+        <Image src={icon} alt={title}
+          width={35} height={35}
+          style={{height: '35px', width: '35px'}}
+        />
         { (title.toLowerCase() == 'cart') && <CartLengthNumber /> }
         <p className="pl-1">{title}</p>
       </a>
